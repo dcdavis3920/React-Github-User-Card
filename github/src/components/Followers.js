1a3b537ea777015588ce0be200e23cfbda23f08f
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-04-09 21:33:14 -0400

    Final Commit

--- github/src/components/Followers.js
+++ github/src/components/Followers.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Card, CardText, CardImg, Col } from "reactstrap";
+import { Card, CardText, CardImg, CardHeader, Col } from "reactstrap";
 
 function Followers(props) {
   console.log(props);
   return (
     <Col sm="12" md={{ size: 6, offset: 3 }}>
       <Card>
+        <CardHeader>GitHub User Info</CardHeader>
         <CardText>{props.data.name}</CardText>
         <CardImg src={props.data.avatar_url} />
         <CardText>Handle: {props.data.login} </CardText>
