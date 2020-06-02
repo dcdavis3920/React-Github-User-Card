e7dbaab1ac3ea2c86c68781aa1edf90c98946695
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-06-02 16:19:01 -0400

    Complete everything is working

--- github/src/components/Followers.js
+++ github/src/components/Followers.js
@@ -6,7 +6,7 @@ function Followers(props) {
   return (
     <Col sm="12" md={{ size: 6, offset: 3 }}>
       <Card>
-        <CardHeader>GitHub Followers</CardHeader>
+        <CardHeader>David's Github Followers</CardHeader>
         <CardText>{props.data.name}</CardText>
         <CardImg src={props.data.avatar_url} />
         <CardText>Handle: {props.data.login} </CardText>
