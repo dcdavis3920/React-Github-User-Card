ac1a4d587728fe2ad1952e47d1a8cfd6e1fc59e0
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-06-02 16:26:12 -0400

    complete

--- github/src/components/UserCard.js
+++ github/src/components/UserCard.js
@@ -6,7 +6,7 @@ function UserCard(props) {
   return (
     <Col sm="12" md={{ size: 6, offset: 3 }}>
       <Card>
-        <CardHeader>GitHub User </CardHeader>
+        <CardHeader>David's GitHub </CardHeader>
         <CardText>{props.data.user.name}</CardText>
         <CardImg src={props.data.user.avatar_url} />
         <CardText>Handle: {props.data.user.login} </CardText>
